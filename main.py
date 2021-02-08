55614f53368c6255bcd7022c1c99abd901485672
khuranamanhar <72623458+khurananmanhar@users.noreply.github.com>
2021-02-08 14:43:46 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+#pip install pyshorteners
+import pyshorteners as pyshorteners
+#pip install PySimpleGUI
+import PySimpleGUI as sg
+sg.theme('DarkBlue12')   
+layout = [  [sg.Text("Enter URL Here")],    
+            [sg.Input()],
+            [sg.Button('Ok')] ]
+window = sg.Window('Window Title', layout)     
+event, values = window.read()         
+popUp = pyshorteners.Shortener().tinyurl.short(values[0]) 
+print(pyshorteners.Shortener().tinyurl.short(values[0]))         
+sg.popup(popUp)
+window.close()      
